603cc659f3d7fd8020071570b93acef6f86ffc92
Eric Domenzain M <ericdomenzain@gmail.com>
2026-06-10 19:41:08 -0600

    add: Configuración de los casos de uso para tareas

--- src/tasks/infrastructure/persistence/task.repository.impl.ts
+++ src/tasks/infrastructure/persistence/task.repository.impl.ts
@@ -4,13 +4,14 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class TaskRepositoryImpl implements ITaskRepository {
+    
     private tasks: Task[] = [];
     
     async create(task: Task): Promise<Task> {
         this.tasks.push(task);
         return task;
     }
-
+    
     async findAll(): Promise<Task[]> {
         return this.tasks;
     }
@@ -18,7 +19,18 @@ export class TaskRepositoryImpl implements ITaskRepository {
     async findById(id: string): Promise<Task | null> {
         return this.tasks.find( t => t.id == id) || null;
     }
+    
+    update(task: Task): Promise<Task> {
+        throw new Error("Method not implemented.");
+    }
 
+    delete(id: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
 }
 
 //! npm i --save class-validator class-transformer
+
+//! git add .
+//! git commit -m "add: Configuración de los casos de uso para tareas"
+//! git push
